0d303065fb96e9953207669db877f6721b5e667e
mkstarr <starrtennis@gmail.com>
2023-10-11 13:52:05 -0700

    bloop circle slider pseudocode

--- blooping_button.js
+++ blooping_button.js
@@ -2,3 +2,11 @@ function bloop(){
     let button = document.getElementsByClassName("blooper")[0];
     button.style.backgroundColor = "red";
 }
+
+/*slide the blooper
+*while click hold on(?(within boundaries of?)) circle
+*move circle to mouse pointer horizontal location
+*set limits of the rounded rectangle on the circle--
+*(account for size of circle if using centerpoint)--
+*sliding
+*/
